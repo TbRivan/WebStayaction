49e79288b87b3efe04aa9c4a4969b7842cafbe21
DESKTOP-EC70NAU\User <tbrivan22@gmail.com>
2022-10-06 20:50:17 +0700

    Initial Commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import moduleName from 'element';
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>LandingPage</div>
+    )
+  }
+}
