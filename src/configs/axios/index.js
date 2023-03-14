49f726e02adaede4b6609b6a2c43c88dfb78b776
Tb Rivan <tbrivan22@gmail.com>
2023-03-14 18:09:28 +0700

    fix env

--- src/configs/axios/index.js
+++ src/configs/axios/index.js
@@ -3,8 +3,8 @@ import axios from "axios";
 import errorResponseHandler from "./errorResponseHandler";
 
 const instance = axios.create({
-  baseURL: `https://web-production-7cea.up.railway.app/api/v1/member`,
-  // baseURL: `${process.env.REACT_APP_HOST}/api/v1/member`,
+  // baseURL: `https://web-production-7cea.up.railway.app/api/v1/member`,
+  baseURL: `${process.env.REACT_APP_HOST}/api/v1/member`,
 });
 
 instance.interceptors.response.use(
